8472cc5e31a7bc25538958d762d94f1c7b6655ff
reach950 <reach950@gmail.com>
2019-05-17 17:24:19 +0800

    添加主页，智能客服的页面元素

---
+++ page_object/AI_customer_service_page.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""智能客服页面"""
+
+__author__ = 'kejie'
+
+from page_object.base_page import BasePage
+from appium.webdriver.common.mobileby import MobileBy
+
+
+class AICustomerServicePage(BasePage):
+
+    # 搜索输入框
+    search_textfield_loc = (MobileBy.CLASS_NAME, 'XCUIElementTypeTextField')
+
+    # 搜索按钮
+    search_button_loc = (MobileBy.IOS_PREDICATE, 'type == "XCUIElementTypeButton" AND rect.width == 28')
+
+    # 输入关键字搜索
+    def search(self, text):
+        self.send_keys(self.search_textfield_loc, text)
+        self.tap_element(self.search_button_loc)
+
+    # 根据name属性检查元素是否存在
+    def check_element_is_display_by_name(self, name):
+        loc = (MobileBy.ACCESSIBILITY_ID, name)
+        return self.find_element(loc).is_displayed()
--- page_object/index_page.py
+++ page_object/index_page.py
@@ -12,8 +12,22 @@ from appium.webdriver.common.mobileby import MobileBy
 
 class IndexPage(BasePage):
 
+    # 我要咨询
+    wo_yao_zi_xun = (MobileBy.IOS_CLASS_CHAIN, '**/XCUIElementTypeImage[`name == "bg_up"`]/XCUIElementTypeOther[3]')
+
+    # 办事指南
+    ban_shi_zhi_nan = (MobileBy.IOS_CLASS_CHAIN, '**/XCUIElementTypeImage[`name == "bg_up"`]/XCUIElementTypeOther[4]')
+
     # 小客车指标
     quota_of_car = (MobileBy.ACCESSIBILITY_ID, '小客车指标')
+
+    # 打开办事指南页面
+    def open_gov_guide_page(self):
+        self.tap_element(self.ban_shi_zhi_nan)
+
+    # 打开智能客服页面
+    def open_AI_customer_service_page(self):
+        self.tap_element(self.wo_yao_zi_xun)
 
     # 打开小客车指标页面
     def open_quota_of_car_page(self):
