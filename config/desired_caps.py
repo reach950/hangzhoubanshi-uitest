8310e70f82d9a5c2d0c66869136f591b57fa924c
reach950 <reach950@gmail.com>
2019-06-05 14:55:27 +0800

    修改desired_caps配置

--- config/desired_caps.py
+++ config/desired_caps.py
@@ -9,12 +9,12 @@ __author__ = 'kejie'
 desired_caps = {
     'automationName': 'XCUITest',  # Xcode8.2以上无UIAutomation,需使用XCUITest
     'platformName': 'iOS',
-    'platformVersion': '12.0',
-    'deviceName': 'kejie',
+    'platformVersion': '11.4.1',
+    'deviceName': '张彬的 iPhone',
     'bundleId': 'com.hzfi.hangzhoubanshi',
-    'udid': '3a5a5974b6d66a4c327a6df99de7f7e0e0a00216',
+    'udid': '89944f20660db25e2ae01d5242a2032e38cb384a',
     'newCommandTimeout': 3600,
     # 'startIWDP': True,
-    # 'xcodeOrgId': 'VHJLLQ36QB',
-    # 'xcodeSigningId': 'iPhone Developer'
+    'xcodeOrgId': '9728DCQYYU',
+    'xcodeSigningId': 'iPhone Developer'
 }
