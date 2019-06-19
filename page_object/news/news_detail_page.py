e9cfd5dfa4e4b511f0ea93d48cbe8bb23c25a532
kejie <reach950@gmail.com>
2019-06-19 15:27:51 +0800

    修改资讯详情的新闻标题的定位器

--- page_object/news/news_detail_page.py
+++ page_object/news/news_detail_page.py
@@ -12,7 +12,7 @@ from page_object.base_page import BasePage
 class NewsDetailPage(BasePage):
 
     # 新闻标题j
-    news_title_loc = (MobileBy.IOS_PREDICATE, 'type == "XCUIElementTypeStaticText" AND value == 2')
+    news_title_loc = (MobileBy.IOS_PREDICATE, '**/XCUIElementTypeWebView/**/XCUIElementTypeStaticText[1]')
 
     # 获取新闻标题
     def get_news_title(self):
