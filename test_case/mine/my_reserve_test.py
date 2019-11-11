78e6252385ae5ee8d66494e20d6dcf35fa620bb6
kejie <reach950@gmail.com>
2019-11-11 15:17:39 +0800

    修改我的-我的预约-预约的测试用例

--- test_case/mine/my_reserve_test.py
+++ test_case/mine/my_reserve_test.py
@@ -6,6 +6,7 @@
 __author__ = 'kejie'
 
 import unittest
+import time
 from test_case.base_case import BaseCase
 from test_case.common_test_step.login import login
 from test_case.common_test_step.reserve import get_reserve_item
@@ -25,6 +26,7 @@ class TestMyReserve(BaseCase):
         """显示行政预约和医疗预约两个tab页"""
         self.index_page.switch_to_mine_page()
         self.mine_page.open_my_reserve()
+        time.sleep(5)
         self.assertTrue(self.my_reserve_page.check_element_by_name('行政预约'))
         self.assertTrue(self.my_reserve_page.check_element_by_name('医疗预约'))
 
@@ -34,9 +36,10 @@ class TestMyReserve(BaseCase):
         reserve_info = get_reserve_item(self.driver)
         self.index_page.switch_to_mine_page()
         self.mine_page.open_my_reserve()
+        time.sleep(5)
         self.assertEqual(self.my_reserve_page.get_first_reserve_name(), reserve_info['预约事项'])
         self.assertEqual(self.my_reserve_page.get_first_reserve_date(), reserve_info['办事时间'])
-        self.assertIn(reserve_info['办事时间'], self.my_reserve_page.get_first_reserve_address())
+        self.assertIn(reserve_info['办事大厅'], self.my_reserve_page.get_first_reserve_address())
 
 
 if __name__ == '__main__':
