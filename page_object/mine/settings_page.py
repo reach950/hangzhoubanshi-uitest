d6b1836524cf774d30cd0f89c4fb91a1be61231b
kejie <reach950@gmail.com>
2019-11-14 16:08:55 +0800

    修改我的-设置-清理缓存的测试用例

--- page_object/mine/settings_page.py
+++ page_object/mine/settings_page.py
@@ -16,7 +16,22 @@ class SettingsPage(BasePage):
     # 退出登录按钮
     logout_button_loc = (MobileBy.ACCESSIBILITY_ID, '退出登录')
 
+    # 清空缓存
+    clear_cache_loc = (MobileBy.ACCESSIBILITY_ID, '清空缓存')
+
+    # 缓存大小
+    cache_size_loc = (MobileBy.IOS_PREDICATE, 'type == "XCUIElementTypeOther" AND rect.width == 61')
+
+    # 清空缓存
+    def clear_cache(self):
+        self.tap_element(self.clear_cache_loc)
+        self.click_alert_button('确定')
+
     # 登出
     def logout(self):
         self.tap_element(self.quit_button_loc)
         self.tap_element(self.logout_button_loc)
+
+    # 获取缓存空间
+    def get_cache_size(self):
+        return self.find_element(self.cache_size_loc).get_attribute('value')
--- test_case/mine/settings_test.py
+++ test_case/mine/settings_test.py
@@ -10,8 +10,8 @@ from test_case.base_case import BaseCase
 from test_case.common_test_step.login import login
 
 
-class TestDeleteCache(BaseCase):
-    """我的-设置-清除缓存"""
+class TestClearCache(BaseCase):
+    """我的-设置-清理缓存"""
 
     def setUp(self):
         super().setUp()
@@ -20,9 +20,12 @@ class TestDeleteCache(BaseCase):
         super().tearDown()
 
     @login
-    def test_01_delete_cache(self):
-        """清楚缓存"""
-        self.assertTrue(True)
+    def test_01_clear_cache(self):
+        """清理缓存，空间显示为0"""
+        self.index_page.switch_to_mine_page()
+        self.mine_page.open_settings_page()
+        self.settings_page.clear_cache()
+        self.assertEqual(self.settings_page.get_cache_size(), '0.00M')
 
 
 if __name__ == '__main__':
